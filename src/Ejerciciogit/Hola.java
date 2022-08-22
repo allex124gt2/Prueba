4adf86714c8fb2f46a9f9d25dde31cd2315900ed
allex <allex124mtz@gmail.com>
2022-08-22 18:26:45 -0500

    Avances

---
+++ src/Ejerciciogit/Hola.java
@@ -0,0 +1,8 @@
+package Ejerciciogit;
+
+public class Hola {
+    public static void main(String []args){
+        System.out.println("hola mundo");
+        System.out.println("Finalizado");
+    }
+}
