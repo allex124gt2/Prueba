5bc406e6ed177e07a8d0f1b3a47da0fe2fd5bffa
allex <allex124mtz@gmail.com>
2022-10-12 20:55:12 -0500

    Avances

--- src/Interface/MarcoSeleccionMultiple/PruebaSeleccionMultiple.java
+++ src/Interface/MarcoSeleccionMultiple/PruebaSeleccionMultiple.java
@@ -1,4 +1,14 @@
 package Interface.MarcoSeleccionMultiple;
-
+import javax.swing.JFrame;
 public class PruebaSeleccionMultiple {
-}
+    public static void main(String[] args) {
+
+        MarcoSeleccionMultiple marcoSeleccionMultiple =
+                new MarcoSeleccionMultiple();
+        marcoSeleccionMultiple.setDefaultCloseOperation(
+                JFrame.EXIT_ON_CLOSE);
+        marcoSeleccionMultiple.setSize(350, 140);
+        marcoSeleccionMultiple.setVisible(true);
+    }
+} // fin de la clase PruebaSeleccionMultiple
+
