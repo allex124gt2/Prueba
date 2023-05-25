90bc344685b073cbdc5aafb7e70acc9ed1030758
allex124gt2 <73039637+allex124gt2@users.noreply.github.com>
2023-05-25 02:16:18 -0600

    Avances

--- src/Ejercicio12042023/videojuegos.java
+++ src/Ejercicio12042023/videojuegos.java
@@ -29,9 +29,6 @@ public class videojuegos {
         return indice;
     }
 
-    public void setIndice(int indice) {
-        this.indice = indice;
-    }
 
     public String getTitulo() {
         return titulo;
@@ -45,66 +42,40 @@ public class videojuegos {
         return plataforma;
     }
 
-    public void setPlataforma(String plataforma) {
-        this.plataforma = plataforma;
-    }
 
     public int getAnio() {
         return anio;
     }
 
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
 
     public String getGenero() {
         return genero;
     }
 
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
 
     public String getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
 
     public double getVentasNa() {
         return ventasNa;
     }
 
-    public void setVentasNa(double ventasNa) {
-        this.ventasNa = ventasNa;
-    }
 
     public double getVentasEu() {
         return ventasEu;
     }
 
-    public void setVentasEu(double ventasEu) {
-        this.ventasEu = ventasEu;
-    }
 
     public double getVentasJp() {
         return ventasJp;
     }
 
-    public void setVentasJp(double ventasJp) {
-        this.ventasJp = ventasJp;
-    }
 
     public double getVentasGl() {
         return ventasGl;
     }
-
-    public void setVentasGl(double ventasGl) {
-        this.ventasGl = ventasGl;
-    }
-
 
 
 }
